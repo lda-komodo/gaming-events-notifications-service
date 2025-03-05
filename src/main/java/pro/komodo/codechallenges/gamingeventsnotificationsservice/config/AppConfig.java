b23d856bf0b69425d47928a241761b43d8509481
Luis Arrieta <luisarrieta85@gmail.com>
2025-03-04 21:26:00 -0500

    basic app structure

---
+++ src/main/java/pro/komodo/codechallenges/gamingeventsnotificationsservice/config/AppConfig.java
@@ -0,0 +1,11 @@
+package pro.komodo.codechallenges.gamingeventsnotificationsservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class AppConfig {
+
+
+}
